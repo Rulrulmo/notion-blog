90f736c63dcaf15a3772dedb3ed7bc0e858737e3
rulmo <hwan901014@gmail.com>
2025-05-23 15:28:40 +0900

    fix: theme provider 수정

--- app/providers.tsx
+++ app/providers.tsx
@@ -18,7 +18,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
 
   return (
     <>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+      <ThemeProvider>
         <QueryClientProvider client={queryClient}>
           {children}
           <ReactQueryDevtools initialIsOpen={false} />
--- components/theme/ThemeProvider.tsx
+++ components/theme/ThemeProvider.tsx
@@ -8,7 +8,7 @@ export function ThemeProvider({
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem>
+    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
       {children}
     </NextThemesProvider>
   );
